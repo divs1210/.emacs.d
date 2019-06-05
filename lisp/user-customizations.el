9e0352a09f566bdcfd8fcd23347937f0d6c36167
Divyansh Prakash <divyansh.prakash@yahoo.com>
2019-06-05 13:41:34 +0530

    projectile bugfix, re-jump

--- lisp/user-customizations.el
+++ lisp/user-customizations.el
@@ -46,9 +46,11 @@
 (require-package 'magit)
 
 
-;; detect project directories
-;; ==========================
+;; projectile
+;; ==========
 (require-package 'projectile)
+(projectile-mode +1)
+(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
 
 
 ;; file explorer
@@ -183,16 +185,38 @@
  '(shell-pop-universal-key "C-t"))
 
 
-;; Projectile
-;; ==========
-(require-package 'projectile)
-(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
-(projectile-mode +1)
-
-
 ;; GC Tuning for elisp
 ;; ===================
 (setq gc-cons-threshold 20000000)
+
+
+;; re-frame jump to keyword
+;; ========================
+(require 'cider-util)
+(require 'cider-resolve)
+(require 'cider-client)
+(require 'cider-common)
+(require 'cider-find)
+(require 'clojure-mode)
+
+(defun re-frame-jump-to-reg ()
+  (interactive)
+  (let* ((kw (cider-symbol-at-point 'look-back))
+         (ns-qualifier (and
+                        (string-match "^:+\\(.+\\)/.+$" kw)
+                        (match-string 1 kw)))
+         (kw-ns (if ns-qualifier
+                    (cider-resolve-alias (cider-current-ns) ns-qualifier)
+                  (cider-current-ns)))
+         (kw-to-find (concat "::" (replace-regexp-in-string "^:+\\(.+/\\)?" "" kw))))
+
+    (when (and ns-qualifier (string= kw-ns (cider-current-ns)))
+      (error "Could not resolve alias \"%s\" in %s" ns-qualifier (cider-current-ns)))
+
+    (progn (cider-find-ns "-" kw-ns)
+           (search-forward-regexp (concat "reg-[a-zA-Z-]*[ \\\n]+" kw-to-find) nil 'noerror))))
+
+(global-set-key (kbd "M->") 're-frame-jump-to-reg)
 
 
 (provide 'user-customizations)
